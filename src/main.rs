d2c090a65411361d263295d06e895a6b187326ab
Davy Duperron <yamafaktory@gmail.com>
2018-09-09 23:12:56 +0200

    More fun 🐼.

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,6 @@ type Selection = Result<Vec<Value>, String>;
 
 fn get_selection(json: &Value, selector: Option<&str>) -> Option<Selection> {
     let mut inner_json = json;
-    let throw = |s, selector: &Vec<&str>, i: usize| -> String {
-        ["Node (", s, ") not found on parent (", selector[i - 1], ")"].join(" ")
-    };
     if let Some(selector) = selector {
         let selector: Vec<&str> = selector.split('.').collect();
         // Returns Result of values or Err early on, stopping the iteration.
@@ -26,10 +23,30 @@ fn get_selection(json: &Value, selector: Option<&str>) -> Option<Selection> {
             .map(|(i, s)| -> Result<Value, String> {
                 if let Ok(index) = s.parse::<isize>() {
                     if (index).is_negative() {
-                        Err("Invalid negative array index".to_string())
+                        Err(String::from("Invalid negative array index"))
                     } else {
                         if inner_json[index as usize] == Value::Null {
-                            Err(throw(s, &selector, i))
+                            let error_message = match inner_json.as_array() {
+                                Some(array) => [
+                                    "Index (",
+                                    s,
+                                    ") is out of bound, node (",
+                                    selector[i - 1],
+                                    ") has a length of",
+                                    &(array.len()).to_string(),
+                                ]
+                                    .join(" "),
+                                // Trying to access an index on a node which
+                                // is not an array.
+                                None => [
+                                    "Node (",
+                                    selector[i - 1],
+                                    ") is not an array",
+                                ]
+                                    .join(" "),
+                            };
+                            println!("# {:?} #", inner_json.as_array());
+                            Err(error_message)
                         } else {
                             inner_json = &inner_json[index as usize];
                             Ok(inner_json.clone())
@@ -37,14 +54,21 @@ fn get_selection(json: &Value, selector: Option<&str>) -> Option<Selection> {
                     }
                 } else {
                     if s.is_empty() {
-                        Err("Unterminated selector found".to_string())
+                        Err(String::from("Unterminated selector found"))
                     } else {
                         if inner_json[s] == Value::Null {
                             if i == 0 {
                                 Err(["Node (", s, ") is not the root element"]
                                     .join(" "))
                             } else {
-                                Err(throw(s, &selector, i))
+                                Err([
+                                    "Node (",
+                                    s,
+                                    ") not found on parent (",
+                                    selector[i - 1],
+                                    ")",
+                                ]
+                                    .join(" "))
                             }
                         } else {
                             inner_json = &inner_json[s];
