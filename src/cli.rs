4a15801a1ec7f0fedb0f8b1bfb9f4d1dcb2fd5a2
Davy Duperron <yamafaktory@gmail.com>
2018-11-03 18:38:09 +0100

    Bump up version 🚀.

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,7 @@
 use clap::{App, Arg, ArgMatches};
 
 // Should match the Cargo.toml version!
-static VERSION: &str = "0.1.7";
+static VERSION: &str = "0.1.8";
 
 /// Get the CLI matches.
 pub fn get_matches<'a>() -> ArgMatches<'a> {
