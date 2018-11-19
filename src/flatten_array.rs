a47783c980792c09c1ce98b52263bdc213ca40dd
Davy Duperron <yamafaktory@gmail.com>
2018-11-19 17:04:33 +0100

    Cleaning ♻️

--- src/flatten_array.rs
+++ src/flatten_array.rs
@@ -5,35 +5,6 @@ use serde_json::Value;
 /// Flatten nested arrays.
 pub fn flatten_array(array: &[Value]) -> Vec<Value> {
     fn deepen(value: Value) -> Value {
-        // let mut r: Vec<Value> = Vec::new();
-        // let t =
-        //     value
-        //         .as_array()
-        //         .unwrap()
-        //         .clone()
-        //         .iter()
-        //         .for_each(move |inner_value| {
-        //             if inner_value.is_array() {
-        //                 deepen(inner_value.clone());
-        //             } else {
-        //                 println!("======>    {:?}", inner_value.clone());
-        //                 r.push(inner_value.clone());
-        //                 //inner_value.clone()
-        //             }
-        //         });
-        // let mut acc: Vec<Value> = Vec::new();
-        // for inner_value in value.as_array().unwrap() {
-        //     if inner_value.is_array() {
-        //         println!("Called again");
-        //         deepen(inner_value.clone());
-        //     } else {
-        //         println!("=> {:?}", inner_value.clone());
-        //         acc.push(inner_value.clone());
-        //         //inner_value.clone()
-        //     }
-        // }
-        // println!("ACC: {:?}", acc);
-
         let t: Vec<Value> = value
             .as_array()
             .unwrap()
