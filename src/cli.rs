111d6671dcc95db806e6b8ea525d8fc394896a0c
Garrett Squire <garrettsquire@gmail.com>
2018-12-04 17:00:53 -0800

    use the clap version macro to simplify setting it in the CLI

--- src/cli.rs
+++ src/cli.rs
@@ -1,28 +1,28 @@
 use clap::{App, Arg, ArgMatches};
 
-// Should match the Cargo.toml version!
-static VERSION: &str = "0.2.2";
-
 /// Get the CLI matches.
 pub fn get_matches<'a>() -> ArgMatches<'a> {
     App::new("jql")
         .about("JSON Query Language")
         .author("Davy Duperron <yamafaktory@gmail.com>")
-        .version(VERSION)
+        .version(crate_version!())
         .arg(
             Arg::with_name("JSON")
                 .help("JSON file to use")
                 .index(1)
                 .required(true),
-        ).arg(
+        )
+        .arg(
             Arg::with_name("selectors")
                 .help("Selectors to apply")
                 .index(2)
                 .required(true),
-        ).arg(
+        )
+        .arg(
             Arg::with_name("inline")
                 .help("Inlines JSON output")
                 .long("inline")
                 .short("i"),
-        ).get_matches()
+        )
+        .get_matches()
 }
