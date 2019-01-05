629160aecbe0e84f7f2298f86728812ebab2ad76
Davy Duperron <yamafaktory@gmail.com>
2019-01-05 10:12:39 +0100

    Try to fix TravicCI not finding the commit reference.

--- after-success.sh
+++ after-success.sh
@@ -3,7 +3,7 @@
 # Clone the repository
 REMOTE_URL="$(git config --get remote.origin.url)";
 cd ${TRAVIS_BUILD_DIR}/.. && \
-git clone ${REMOTE_URL} "${TRAVIS_REPO_SLUG}-bench" && \
+git clone ${REMOTE_URL} "${TRAVIS_REPO_SLUG}-bench" --depth 10 && \
 cd  "${TRAVIS_REPO_SLUG}-bench" && \
 
 # Bench master
