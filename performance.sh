31f78dbe2a2dc5543645858b57dfb496c3c0637a
yamafaktory <yamafaktory@gmail.com>
2020-06-25 23:03:26 +0200

    chore: increase bench runs

--- performance.sh
+++ performance.sh
@@ -2,6 +2,7 @@
 REPORT=$(pwd)/PERFORMANCE.md
 PERFORMANCE_TMP_DIR=$(pwd)/performance_tmp
 MD_FILES="$PERFORMANCE_TMP_DIR/"*".md"
+MIN_RUNS=1000
 
 # Remove export file if present.
 rm -f $REPORT
@@ -12,19 +13,19 @@ mkdir $PERFORMANCE_TMP_DIR
 # Run the benchmarks.
 hyperfine \
     --export-markdown "$PERFORMANCE_TMP_DIR/OBJECT.md" \
-    --min-runs 2 \
+    --min-runs $MIN_RUNS \
     "echo '{ \"foo\": \"bar\" }' | jq '.foo'" \
     "echo '{ \"foo\": \"bar\" }' | jql '."foo"'"
 
 hyperfine \
     --export-markdown "$PERFORMANCE_TMP_DIR/ARRAY_INDEX.md" \
-    --min-runs 2 \
+    --min-runs $MIN_RUNS \
     "echo '[1, 2, 3]' | jq '.[0]'" \
     "echo '[1, 2, 3]' | jql '.[0]'"
 
 hyperfine \
     --export-markdown "$PERFORMANCE_TMP_DIR/ARRAY_FLATTEN.md" \
-    --min-runs 2 \
+    --min-runs $MIN_RUNS \
     "echo '[1, [2], [[3]]]' | jq 'flatten'" \
     "echo '[1, [2], [[3]]]' | jql '...'"
 
