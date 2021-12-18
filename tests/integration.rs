004ea5184ae793807ba5a832cdedc77a39bcede9
yamafaktory <yamafaktory@gmail.com>
2021-12-18 13:16:49 +0100

    feat(lib): make clap and colored_json optional

---
+++ tests/integration.rs
@@ -0,0 +1,12 @@
+#![deny(unsafe_code, nonstandard_style)]
+#![forbid(rust_2018_idioms)]
+
+use jql::walker;
+use serde_json::json;
+
+#[test]
+fn integration() {
+    let json_array = json!([2, 3, 5, 7, 11]);
+
+    assert_eq!(walker(&json_array, Some("[4]")), Ok(json!(11)));
+}
