4d3e7c4a7886ba671276a0c489faa3ac979ac85e
Davy Duperron <yamafaktory@gmail.com>
2023-04-23 18:58:15 +0200

    Chore/documentation and licenses (#212)
    * chore(docs): update docs and add licenses
    
    * chore(docs): add emoji
    
    * fix(docs): use direct svg URL

--- crates/jql-runner/src/lib.rs
+++ crates/jql-runner/src/lib.rs
@@ -3,6 +3,9 @@
 #![forbid(rust_2021_compatibility)]
 #![warn(missing_debug_implementations, missing_docs, unreachable_pub)]
 #![doc = include_str!("../README.md")]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/yamafaktory/jql/a438ab0039faf64c3329fb09bae399ae95601000/jql.svg"
+)]
 
 /// Array utilities.
 mod array;
