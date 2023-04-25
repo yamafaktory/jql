3a9f85a61f648558c724f7829aa6433fea7179e9
Davy Duperron <yamafaktory@gmail.com>
2023-04-25 12:03:01 +0200

    test(perf): adjust spec

--- test.sh
+++ test.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
 LARGE_JSON_FILE=$(pwd)/assets/github-repositories.json
-MIN_RUNS=10
+RELEASE=$(pwd)/target/release/jql
+MIN_RUNS=100
 
 hyperfine \
     --min-runs $MIN_RUNS \
-    "cat $LARGE_JSON_FILE | jq -r '[.[] | {name: .name, full_name: .full_name}]' > /dev/null" \
-    "cat $LARGE_JSON_FILE | jql '|>{\"name\", \"full_name\"}' > /dev/null"
-
-hyperfine \
-    --min-runs $MIN_RUNS \
-    "cat $LARGE_JSON_FILE | jq -r '[.[] | {name: .name, full_name: .full_name}]' > /dev/null" \
-    "cat $LARGE_JSON_FILE | ./target/release/jql '|>{\"name\", \"full_name\"}' > /dev/null"
+    "cat $LARGE_JSON_FILE | jql '|>{\"name\",\"full_name\"}' > /dev/null" \
+    "cat $LARGE_JSON_FILE | $RELEASE '|>{\"name\",\"full_name\"}' > /dev/null"
